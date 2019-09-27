4287746e275672080b3565ee70286d485fe55e16
jennifferplaza <jenniffer.plaza.aliaga@gmail.com>
2019-09-27 20:58:15 -0300

    realice actividad hasta la pregunta número 6

---
+++ DesarrolloPreguntas.R
@@ -0,0 +1,30 @@
+#lista de numeros
+listaDeNumeros <- list(2,5,6,2,1,5,6,10,11,20,15)
+listaDeNumeros[5]
+listaDeNumeros[0]
+listaDeNumeros[12]
+listaDeNumeros[-5]
+unlist(listaDeNumeros[5])
+unlist(listaDeNumeros[1])
+if(listaDeNumeros[5]+1>0){
+  print("se cumple")
+}
+if(unlist(listaDeNumeros[5])+1>0){
+  print("se cumple")
+}
+listaDeNumeros[5] <- 12
+length(nombre_variable)
+length(listaDeNumeros)
+valorInicial <-5
+valorFinal <- 20
+valorInicial :
+  valorFinal
+valorInicial <- 11
+valorFinal <-5
+valorInicial :
+  valorFinal
+length(listaDeNumeros):
+  valorFinal
+for(i in 1 : 100){
+  print(paste("cuento ",i," misisipis"))
+}
