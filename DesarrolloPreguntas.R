fa67c79f025fbf129ab8b2e894a15a02ef22d3d2
Rafaelgz <gemelorafael@gmail.com>
2019-09-29 09:52:55 -0300

    Arreglando

--- DesarrolloPreguntas.R
+++ DesarrolloPreguntas.R
@@ -16,7 +16,7 @@ listaDeNumeros[5] <- 12
 length(nombre_variable)
 length(listaDeNumeros)
 valorInicial <-5
-valorFinal <- 20
+valorFinal <- 11
 valorInicial :
   valorFinal
 valorInicial <- 11
@@ -28,3 +28,69 @@ length(listaDeNumeros):
 for(i in 1 : 100){
   print(paste("cuento ",i," misisipis"))
 }
+for(i in listaDeNumeros){
+  print(paste("cuento",i,"misisipi"))
+}
+for(i in listaDeNumeros){
+  print(paste(i))
+}
+pares <- c()
+impares <- c()
+for(i in listaDeNumeros){
+  if(i%%2==0){
+    print("pares")
+  }else{
+    print("impar")
+  }
+}
+funcionElectoral<-function(total,votosSi,votosNo)
+  votosSi<-24
+votosNo<-20
+total<-44
+if((votosSi+votosNo)>=(total*0.5+1)&&(votosSi>votosNo)){
+  print("GANA EL Si")
+}else if((votosSi+votosNo)>=(total*0.5+1)&&(votosNo=votosSi)){
+  print("GANA EL NO")
+}else if((votosSi+votosNo)>=(total*0.5+1)&&(votosNo>votosSi)){
+  print("GANA EL No")
+}else if((votosSi+votosNo)<(total*0.5+1)&&(votosSi>=(total*0.3))){  
+  print("GANA EL Si")
+}else if((votosSi+votosNo)<(total*0.5+1)&&(votosNo>=(total*0.3))){
+  print("GANA EL No")
+}else{
+  print("GANA EL No")
+}
+
+Suma<-function(a,b){
+  a+b
+}
+Suma(3,4)
+
+resta<-function(a,b){
+  a-b
+}
+resta(3,4)  
+multiplicacion<-function(a,b){
+  a*b
+}
+multiplicacion(3,4)
+
+division<-function(a,b){
+  a/b
+}
+division(3,4)
+
+restaDeAreasRectangulo<-function(b1,b2,h1,h2){
+  (b1*h1)-(b2*h2)
+}
+restaDeAreasRectangulo(10,5,4,2)
+
+restaDeAreasCirculo<-function(pi,r1,r2){
+  (pi*r1^2)-(pi*r2^2)
+}
+restaDeAreasCirculo(3.14,7,5)
+
+for( i in listaDeNumeros){
+  print(i)
+}
+
